c7f90f492ece202366ae8e6343de03b052924ac4
Rafiita28 <158265241+Rafiita28@users.noreply.github.com>
2025-02-24 13:22:51 +0100

    Update AreaCalculator.java

--- AreaCalculator.java
+++ AreaCalculator.java
@@ -1,13 +1,21 @@
 public class AreaCalculator {
     public static void main(String[] args) {
         System.out.println("Calculadora de área para diferentes figuras.");
+        
+        System.out.println("Área del círculo: " + circleArea(5));
+        System.out.println("Área del rectángulo: " + rectangleArea(4, 6));
+        System.out.println("Área del triángulo: " + triangleArea(4, 6));
     }
 
     public static double circleArea(double radius) {
         return Math.PI * radius * radius;
     }
-    // Añadir métodos para calcular el área de un rectángulo y un triángulo
+
     public static double rectangleArea(double base, double height) {
         return base * height;
+    }
+
+    public static double triangleArea(double base, double height) {
+        return (base * height) / 2;
     }
 }
